654330094558a47cc3f94ad0c3bf18968841ae5d
MakiWinster <makiiiiiiiho@gmail.com>
2026-02-11 00:57:51 +0800

    feat: 添加prompt修改入口

--- ui/src/common/interface.ts
+++ ui/src/common/interface.ts
@@ -838,6 +838,10 @@ export interface AiConfig {
     api_key: string;
     model: string;
   }>;
+  prompt_config?: {
+    zh_cn: string;
+    en_us: string;
+  };
 }
 
 export interface AiProviderItem {
